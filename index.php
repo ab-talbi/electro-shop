65e22edb2d64696d1bb3bbe2ecfe706a51d21ec7
Smail ID YASSINE <ismailidyassin@gmail.com>
2022-05-24 00:16:51 +0100

    update footer and navbar

--- index.php
+++ index.php
@@ -13,52 +13,15 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
    
     <!-- css file -->
-    <link rel="stylesheet" href="./css/style.css">
-
     <link rel="stylesheet" href="./css/style.css?v=<?php echo time(); ?>">
 
 </head>
 <body>
     
     <!-- NavBar -->
-    <div class="container-fluid p-0">
-        <nav class="navbar navbar-expand-lg navbar-light" style="background:#f39c12;">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#"><img src="./images/128.png" alt="logo" class="logo"></a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Acceuil</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Produits</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Connexion</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Contact</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#"><i class="fa-solid fa-cart-arrow-down"><sup>1</sup></i></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Total Prix: 100 MAD</a>
-                    </li>
-                </ul>
-                <form class="d-flex">
-                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-outline-light" type="submit">Search</button>
-                </form>
-            </div>
-        </div>
-        </nav>
-    </div>
-    
-
+    <?php
+        include("./navbar.php");
+    ?>
 
     <nav class="navbar navbar-expand-lg navbar-dark bg-blue-black">
         <ul class="navbar-nav me-auto">
@@ -183,7 +146,7 @@
 
     <!-- Footer -->
     <?php
-        include_once("./footer.php");
+        include("./footer.php");
     ?>
 
     <!-- bootstrap-JS -->
