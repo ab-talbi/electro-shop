9cf5a61ac6171af227ba3a5e263348c8acc4b4ab
ayoub <ayoubtalbi787@gmail.com>
2022-05-23 22:29:42 +0100

    electro shop juste une petite modification

--- index.php
+++ index.php
@@ -16,7 +16,6 @@
     
 
 
-
     <nav class="navbar navbar-expand-lg navbar-dark bg-blue-black">
         <ul class="navbar-nav me-auto">
             <li class="nav-item">
@@ -80,7 +79,7 @@
                     <div class="card">
                         <img src="./images/samsung50.jpeg" class="card-img-top" alt="...">
                         <div class="card-body">
-                            <h5 class="card-title">Samsung 50 4K UHD Smart TV (TU8500)</h5>
+                            <h5 class="card-title">Samsung 50 4K UHD Smart TV</h5>
                             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                             <a href="#" class="btn btn-primary">Ajouter</a>
                             <a href="#" class="btn btn-secondary">Voir les détails</a>
