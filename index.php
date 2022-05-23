94b853bc148e1a4a8b69880aa9ad7a0f72a4a812
ayoub <ayoubtalbi787@gmail.com>
2022-05-23 18:41:33 +0100

    electro shop version 1.0.0

---
+++ index.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Electro Shop - Acceuil</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+    
+
+
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-secondary">
+        <ul class="navbar-nav me-auto">
+            <li class="nav-item">
+                <a href="" class="nav-link">Bienvenue</a>
+            </li>
+            <li class="nav-item">
+                <a href="" class="nav-link">Connexion</a>
+            </li>
+        </ul>
+    </nav>
+    
+    <div class="bg-light">
+        <h3 class="text-center">Electro Shop</h3>
+        <p class="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, aspernatur!</p>
+    </div>
+
+
+    <div class="row">
+        <div class="col-md-2">
+
+        </div>
+        <div class="col-md-10">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="card">
+                        <img src="./images/samsung50.jpeg" class="img-fluid card-img-top" alt="..."  >
+                        <div class="card-body">
+                            <h5 class="card-title">Samsung 50 4K UHD Smart TV (TU8500)</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Ajouter</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card">
+                        <img src="./images/appleimac.jpeg" class="img-fluid card-img-top" alt="..." >
+                        <div class="card-body">
+                            <h5 class="card-title">Apple iMac A1418 21.5</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Ajouter</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card">
+                        <img src="./images/samsung50.jpeg" class="card-img-top" alt="..." >
+                        <div class="card-body">
+                            <h5 class="card-title">Samsung 50 4K UHD Smart TV (TU8500)</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Ajouter</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+    </div>
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+</body>
+</html>
