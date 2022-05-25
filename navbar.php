0cd86494cc183c0a6c28171796e65a1bf50188f1
ayoub <ayoubtalbi787@gmail.com>
2022-05-25 14:35:56 +0100

    electro test

--- navbar.php
+++ navbar.php
@@ -1,40 +1,34 @@
-
-<<<<<<< HEAD
-<div class="container-fluid p-0 "  style="background:#f39c12">
-        <nav class="navbar navbar-expand-lg navbar-light" >
-=======
 <div class="container-fluid navPrincipal p-0" style="background:#f39c12;">
         <nav class="navbar navbar-expand-lg navbar-light">
->>>>>>> 2e1fb587cfad2d67dee2cfbe944efab8d3d3bddb
             <div class="container-fluid">
                 <a class="navbar-brand" href="#"><img src="./images/128.png" alt="logo" class="logo"></a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
-                    </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Acceuil</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Produits</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Contact</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#"><i class="fa-solid fa-cart-arrow-down"><sup>1</sup></i></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Total Prix: 100 MAD</a>
-                    </li>
-                </ul>
-                <form class="d-flex">
-                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-outline-light" type="submit">Search</button>
-                </form>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Acceuil</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Produits</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Contact</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#"><i class="fa-solid fa-cart-arrow-down"><sup>1</sup></i></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Total Prix: 100 MAD</a>
+                        </li>
+                    </ul>
+                    <form class="d-flex">
+                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                        <button class="btn btn-outline-light" type="submit">Search</button>
+                    </form>
+                </div>
             </div>
-        </div>
         </nav>
-    </div>
+</div>
     
