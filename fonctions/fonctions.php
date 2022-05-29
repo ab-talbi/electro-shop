b30e154fc2f60debb210e24346f18e50551bd197
ayoub <ayoubtalbi787@gmail.com>
2022-05-29 17:38:32 +0100

    electro shop ajout de la fonction de ip

--- fonctions/fonctions.php
+++ fonctions/fonctions.php
@@ -158,4 +158,22 @@
         }
     }
 
+
+    function getIPAddress() {  
+        //whether ip is from the share internet  
+        if(!empty($_SERVER['HTTP_CLIENT_IP'])) {  
+            $ip = $_SERVER['HTTP_CLIENT_IP'];  
+        }  
+        //whether ip is from the proxy  
+        elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {  
+            $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];  
+        }  
+        //whether ip is from the remote address  
+        else{  
+            $ip = $_SERVER['REMOTE_ADDR'];  
+        }  
+         
+        return $ip;  
+    }
+
 ?>
