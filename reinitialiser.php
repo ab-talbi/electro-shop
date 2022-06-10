92e512ffb750d4ddc8b476cde4eaee8d06e76300
Smail ID YASSINE <ismailidyassin@gmail.com>
2022-06-10 02:33:15 +0100

    add: R Ex pour registre

--- reinitialiser.php
+++ reinitialiser.php
@@ -1,7 +1,6 @@
 <?php
     session_start();
-    unset($_SESSION['nom_utilisateur']);
     unset($_SESSION['values']);
     unset($_SESSION['erreurs']);
-    header('Location: index.php');
+    header('Location: registre.php');
 ?>
